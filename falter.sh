6cbe8e0bb9859cdaee97cad9e829e2a6edaa67cd
Martin Hübner <martin.hubner@web.de>
2020-09-06 22:47:17 +0200

    falter.sh: adjust workflow for building falter against different branches than master.

--- falter.sh
+++ falter.sh
@@ -3,10 +3,12 @@
 # This script implements the tutorial given at http://10.31.44.119:10178/compiling-falter-images-for-freifunk-berlin/
 # (only accessible in Freifunk-Berlin-net) from kls0e in a somehow automatted way. Falter, live long and prosperous. ;-)
 
-THREADS=9 # amount of cpu-threads +1
+# get amount of cpu-threads +1
+THREADS_TMP=$(lscpu | grep "CPU(s):" | head -n 1 | rev | cut -d' ' -f 1)
+THREADS=$(($THREADS_TMP + 1))
 DIFFCONFIG=$(cat diff_config_freifunk)
 BUILDDIR="falter-firmware"
-VERSION="v0.0.4"
+VERSION="v0.0.5"
 
 set -e
 
@@ -15,28 +17,35 @@ update_owrt() {
     ./scripts/feeds install -a
 }
 
-init_buildsystem() {
-    git clone https://git.openwrt.org/openwrt/openwrt.git $BUILDDIR
-    cd $BUILDDIR
-    cp feeds.conf.default feeds.conf
-
+add_falter_feed() {
     # check if already included
+    cp feeds.conf.default feeds.conf
     if [ "$(grep 'src-git falter https://github.com/Freifunk-Spalter/packages.git' feeds.conf)" != 0 ]; then
         echo "src-git falter https://github.com/Freifunk-Spalter/packages.git" >>feeds.conf
     fi
+}
+
+init_buildsystem() {
+    git clone https://git.openwrt.org/openwrt/openwrt.git $BUILDDIR
+    cd $BUILDDIR
+    add_falter_feed
     update_owrt
 }
 
-update() {
+update_source() {
     cd $BUILDDIR
     git pull
     update_owrt
 }
 
-after_branch_change() {
+branch_change() {
     cd $BUILDDIR
     echo "performing distclean on $BUILDDIR..."
     make distclean
+    echo "switching branch to $1..."
+    git checkout $1
+    add_falter_feed
+    update_owrt
 }
 
 write_diffconfig() {
@@ -45,10 +54,14 @@ write_diffconfig() {
     if [ -e diffconfig ]; then
         rm diffconfig && touch diffconfig
     fi
+    ./scripts/diffconfig.sh > diffconfig
+
+    # write diff for freifunk-packages
     for FLAG in $DIFFCONFIG; do
         echo "$FLAG" >>diffconfig
     done
-    cat diffconfig >>.config
+    cp diffconfig .config
+    #cat diffconfig >>.config
     make defconfig
 }
 
@@ -66,16 +79,17 @@ case $1 in
     "-m" | "--make")
         cd $BUILDDIR
         echo "start building falter with $THREADS threads..."
-        make -j$THREADS V=s
+        make -j$THREADS V=sc
     ;;
     "-u" | "--update")
-        update
+        update_source
     ;;
     "-b" | "--branch")
-        after_branch_change
+        TAG=$2
+        branch_change $TAG
     ;;
     *)
-    echo -e "Falter-script $VERSION
+    echo -e "Falter-build-script $VERSION
 To build falter-images, execute the options in this order.
 
 -i | --init\tinitialise buildsystem
@@ -84,13 +98,13 @@ To build falter-images, execute the options in this order.
 -m | --make\tstart building
 
 optional:
--u | --update\tupdates with new patches from falter-repo.
-\t\tAfter using this option, you need to start
-\t\tagain at option -s.
--b | --branch\tif you want to build falter on another
-\t\tOpenWrt-branch than master, check out that
-\t\tbranch via git. Afterwards run this option
-\t\tand start again at -w.
+-u | --update\t\tupdates with new patches from falter-repo.
+\t\t\tAfter using this option, you need to start
+\t\t\tagain at option -s.
+-b | --branch [\$BRANCH]\tif you want to build falter on a different
+\t\t\tOpenWrt-branch than master, give the branch
+\t\t\tor tag. After executing this option start
+\t\t\tagain at -s.
 
 EVER use only one option at the same time!"
     ;;
